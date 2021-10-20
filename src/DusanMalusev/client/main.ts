ecbdf8da188988319daabeadb47bfb887b204a68
Dusan Malusev <malusevd99@gmail.com>
2021-10-21 00:34:25 +0200

    Adding logrocket
    Signed-off-by: Dusan Malusev <malusevd99@gmail.com>

--- src/DusanMalusev/client/main.ts
+++ src/DusanMalusev/client/main.ts
@@ -1,5 +1,7 @@
 import './css/index.css';
 
+import LogRocket from 'logrocket';
+
 import {CsrfToken} from './csrf';
 import Http from './http';
 import Sidebar from './sidebar';
@@ -12,7 +14,8 @@ document.addEventListener('DOMContentLoaded', () => {
     Http.baseUrl = process.env.API_URL;
     Http.csrf = csrfToken;
     setKey(process.env.RECAPTCHA_FRONTEND_URL);
-
+    LogRocket.init(process.env.LOG_ROCKET);
+    
     sidebar.attachToElement();
 });
 
